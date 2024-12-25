4176b8f5e84947d020bc0b1ef541b9a5102c0a74
Deepak Raj <drdeepakraj03pc@gmail.com>
2024-12-25 10:52:47 +0530

    Add files via upload

---
+++ age.js
@@ -0,0 +1,35 @@
+const BtnEl = document.getElementById("Btn");
+const BirthdayEl = document.getElementById("birthdate");
+const resultEl = document.getElementById("result");
+
+function CalculateAge(){
+    const birthdayValue = BirthdayEl.value;
+    if (birthdayValue === ""){
+        alert("Plese Enter Valid Date")
+    }else{
+        const age = getAge(birthdayValue); 
+        resultEl.innerText = `Your age is ${age} ${age > 1 ? "year": "years"}old`;
+    }
+
+}
+
+function getAge(birthdayValue){
+    const currentdate = new Date();
+    const birthdate = new Date(birthdayValue);
+    const age = currentdate.getFullYear() - birthdate.getFullYear();
+    
+    const month = currentdate.getMonth() -birthdate.getMonth();
+    if (
+        month < 0 ||
+        (month === 0 && currentdate.getDate() < birthdate.getDate())
+      ) {
+        age--;
+      }
+      return age;
+
+      console.log(age)
+};
+
+
+
+BtnEl.addEventListener("click",CalculateAge);
